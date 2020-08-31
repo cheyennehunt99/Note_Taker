3d72425511b1fa23b63fc83fe4e1fcb9edda5558
Chien81 <cheyennehunt78@gmail.com>
2020-08-30 18:37:29 -0700

    added api & html routes

---
+++ Routes/htmlRoutes.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = app => {
+
+        // Display notes.html when /notes is accessed
+        app.get('/notes', function(req,res) {
+            res.sendFile(path.join(__dirname, "../notes.html"));
+        });
+        
+        // Display index.html when all other routes are accessed
+        app.get('*', function(req,res) {
+            res.sendFile(path.join(__dirname, "../index.html"));
+        });
+
+        //updates the json file whenever a note is added or deleted
+        function updateDb() {
+            fs.writeFile("db/db.json",JSON.stringify(notes,'\t'),err => {
+                if (err) throw err;
+                return true;
+            });
+        }
+
+    });
+
+}
